8db3dedd9819cdb8d8d464fecd9d59000ce63af4
Sinan Adlim <sinanadlim@gmail.com>
2015-11-18 21:56:49 -0500

    Siyah Coming Soon dosyaları
    times new romanlı coming soon siyah üstüne beyaz

---
+++ Coming_Soon_files/Coming_Soon.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20151118
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,756),url:'Coming_Soon_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Coming_Soon_files/stroke_1.png'},{rect:new IWRect(1,-1,881,2),url:'Coming_Soon_files/stroke_2.png'},{rect:new IWRect(882,-1,2,2),url:'Coming_Soon_files/stroke_3.png'},{rect:new IWRect(882,1,2,756),url:'Coming_Soon_files/stroke_4.png'},{rect:new IWRect(882,757,2,2),url:'Coming_Soon_files/stroke_5.png'},{rect:new IWRect(1,757,881,2),url:'Coming_Soon_files/stroke_6.png'},{rect:new IWRect(-1,757,2,2),url:'Coming_Soon_files/stroke_7.png'}],new IWSize(883,758))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Coming_Soon_files/Coming_SoonMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
